6ab31941316895965fbaecf587ac7ba95002bfb6
Juan Andres <ja.mendez@uniandes.edu.co>
2020-04-07 21:21:27 -0500

    Taller 2

---
+++ dataCourses.ts
@@ -0,0 +1,12 @@
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo de SW en equipo", "Jose Joaquin Bocanegra Garcia", 3),
+  new Course("Sistemas transaccionales", "Claudia Lucia Jimenez Guarin", 3),
+  new Course("Matemática Estructural", "Carlos Augusto Di Prisco De Venanzi", 3),
+  new Course("Ecuaciones Diferenciales", "Milkhail Malakhalsev", 3),
+  new Course("Proyecto Intermedio", "Carlos Francisco Rodriguez Herrera", 3),
+  new Course("Mecánica de fluidos", "Omar Dario Lopez Mejia", 3),
+  new Course("Laboratorio de mecánica de fluidos", "Omar Dario Lopez Mejia", 0),
+  new Course("Peligros Naturales", "Natalia Pardo Villaveces", 2)
+]
